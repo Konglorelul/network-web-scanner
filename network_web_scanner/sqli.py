de798652c07d2f70b2c92f66e570dcff9fce6306
Voicu Vlad <voicuvlad1802@gmail.com>
2025-05-29 21:42:02 +0300

    GUI 6

--- network_web_scanner/sqli.py
+++ network_web_scanner/sqli.py
@@ -2,3 +2,9 @@ def sqli(URL, output):
     print("test sqli")
     print("URL: "+URL)
     print("ot: "+str(output))
+
+    payload = "1=1"
+    injected_url = ""
+    payload_index = URL.find("[*]")
+    injected_url = URL[0:payload_index] + payload #+ URL[payload_index+3:len(URL)]
+    print(injected_url)
